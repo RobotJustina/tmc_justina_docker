01af56cf15bd46e9291a18a380696e72badfb98a
alberto <albertoemru@gmail.com>
2021-06-23 18:47:51 -0500

    Cambios en maquina de estados task_1

--- src/planning/act_pln/src/task_1.cpp
+++ src/planning/act_pln/src/task_1.cpp
@@ -22,35 +22,38 @@ enum SMState {
     SM_WHERE_IS,
     SM_SELECT_OBJECT_GRASP,
 	SM_NAVIGATE_TO_INSPECTION,
-    SM_NAVIGATE_LOCATION,
+    SM_LEFT_NAVIGATE_LOCATION,
+    SM_RIGHT_NAVIGATE_LOCATION,
 	SM_ALIGN_TABLE,
 	SM_DETECT_OBJECT,
     SM_NO_DETECT_OBJECT,
 	SM_HANDLER,
 	SM_GRASP_OBJECT,
-	SM_DELIVER_OBJECT,
+	SM_RIGHT_DELIVER_OBJECT,
+    SM_LEFT_DELIVER_OBJECT,
 	SM_REPEAT_TASK,
 	SM_FINAL_STATE
 };
 
 //delivery locations for each category
-enum objType {None,Food, Kitchen, Tools, Shape};
+enum objType {None,Food, Kitchen, Tool, Shape};
 
 std::string get_location(objType type) {
    switch(type) {
       case Food:
          return "long_table_a_2";
       case Kitchen:
-         return "Container_A";
-      case Tools:
-         return "Drawer_top";
+         return "long_table_a_1";
+      case Tool:
+         return "bin_a";
       case Shape:
-         return "Drawer_left";
+         return "bin_a";
       default:
-         return "Invalid type";
+         return "bin_b";
    }
 }
 //-------arms
+objType obj_type=None;
 //right
 int right_obj_index=-1;
 objType right_obj_type=None;
@@ -99,8 +102,7 @@ int main(int argc, char** argv){
 			    //go to tall_table because is the easiest path
                 if(!JustinaNavigation::getClose("tall_table", 120000)){
                     std::cout << "Cannot move to tall_table" << std::endl;
-                }
-                
+                }                
                 state = SM_ALIGN_TABLE;
 			    
         	    break;	
@@ -122,72 +124,116 @@ int main(int argc, char** argv){
                 if(JustinaVision::detectAllObjectsVot(recoObj, image, 5)){
                     for(int j = 0; j < recoObj.size() && !table_aligned; j++){
                         // id.rfind("",0) search by prefix
-                        //Food category
+                        //each category
                         if (recoObj[j].id.rfind("Food", 0) == 0){
-                            //select arm
-                            //left
-                            if(recoObj[index].pose.position.y > 0 && left_obj_index<0){
-                                left_obj_type=Food;
-                                left_obj_index=j;
-                                std::cout<< "Selecting "<<recoObj[j].id<<" with left arm"<<std::endl;
-                            }
-                            //right
-                            else if(right_obj_index<0){
-                                right_obj_type=Food;
-                                right_obj_index=j;
-                                std::cout<< "Selecting "<<recoObj[j].id<<" with right arm"<<std::endl;
-                            }
+                            obj_type=Food;
+                        }else if (recoObj[j].id.rfind("kitchen", 0) == 0){
+                            obj_type=Kitchen;
+                        }else if (recoObj[j].id.rfind("shape", 0) == 0){
+                            obj_type=Shape;
+                        }else if (recoObj[j].id.rfind("tool", 0) == 0){
+                            obj_type=Tool;
                         }
+                        //select arm
+                        //left
+                        if(recoObj[j].pose.position.y > 0 && left_obj_index<0){
+                            left_obj_type=obj_type;
+                            left_obj_index=j;
+                            std::cout<< "Selecting "<<recoObj[j].id<<" with left arm"<<std::endl;
+                        }
+                        //right
+                        else if(right_obj_index<0){
+                            right_obj_type=obj_type;
+                            right_obj_index=j;
+                            std::cout<< "Selecting "<<recoObj[j].id<<" with right arm"<<std::endl;
+                        }
+                        
                     }
                 } 
-	        	state = SM_GRASP_OBJECT;
+                if(right_obj_index<0 && left_obj_index<0){
+                    //small move to try again...
+                    state = SM_ALIGN_TABLE;
+                }else{
+	        	    state = SM_GRASP_OBJECT;
+                }
     		    break;
             //if and object has been detected try to grasp it
             case SM_GRASP_OBJECT:
 			    std::cout << "State machine: SM_GRASP_OBJECT" << std::endl;
 
                 //try to grasp objects with selected arm
-                
+                JustinaManip::torsoGoTo(0,0,0,5000);
                 if(left_obj_index>=0){
-                    JustinaTasks::graspObject(recoObj[left_obj_index].pose.position.x, recoObj[left_obj_index].pose.position.y, recoObj[left_obj_index].pose.position.z, true, "", true);
-                    drop = false;                		
+                    JustinaTasks::graspObject(recoObj[left_obj_index].pose.position.x, recoObj[left_obj_index].pose.position.y, recoObj[left_obj_index].pose.position.z, true, "", false);
+                    state = SM_LEFT_NAVIGATE_LOCATION;               		
                 }
                 if(right_obj_index>=0){
-                    JustinaTasks::graspObject(recoObj[right_obj_index].pose.position.x+0.2, recoObj[right_obj_index].pose.position.y, recoObj[right_obj_index].pose.position.z, false, "", true);
-                    drop = true;
+                    JustinaTasks::graspObject(recoObj[right_obj_index].pose.position.x, recoObj[right_obj_index].pose.position.y, recoObj[right_obj_index].pose.position.z, false, "", true);
+                    if (state != SM_LEFT_NAVIGATE_LOCATION){
+                        state = SM_RIGHT_NAVIGATE_LOCATION;  
+                    }
                 }
-                
-				state = SM_NAVIGATE_LOCATION;		        
+                //if no obj grasped
+
+                /*
+				if (state!= SM_RIGHT_NAVIGATE_LOCATION && state!= SM_LEFT_NAVIGATE_LOCATION ){
+                    state= SM_DETECT_OBJECT;		        
+        		    std::cout << "No object grasped" << std::endl;
+                }*/
+
 			    break;
             //go to deliver zone
-            case SM_NAVIGATE_LOCATION:
+            case SM_LEFT_NAVIGATE_LOCATION:
         		//Go to location
-        		std::cout << "State machine: SM_NAVIGATE_LOCATION" << std::endl;
+        		std::cout << "State machine: SM_LEFT_NAVIGATE_LOCATION" << std::endl;
+                //std::string target = get_location(right_obj_type);
+                std::cout << "Moving to " << get_location(left_obj_type)<< std::endl;
+                if(!JustinaNavigation::getClose(get_location(left_obj_type), 120000)){
+                    std::cout << "Cannot move to "<<get_location(left_obj_type) << std::endl;
+                }
+                JustinaNavigation::moveDist(0.4, 4000);
+                state = SM_LEFT_DELIVER_OBJECT;
+        	    break;
+            case SM_RIGHT_NAVIGATE_LOCATION:
+        		//Go to location
+        		std::cout << "State machine: SM_RIGHT_NAVIGATE_LOCATION" << std::endl;
                 //std::string target = get_location(right_obj_type);
                 std::cout << "Moving to " << get_location(right_obj_type)<< std::endl;
                 if(!JustinaNavigation::getClose(get_location(right_obj_type), 120000)){
                     std::cout << "Cannot move to "<<get_location(right_obj_type) << std::endl;
                 }
                 JustinaNavigation::moveDist(0.4, 4000);
-                state = SM_DELIVER_OBJECT;
-			    
+                state = SM_RIGHT_DELIVER_OBJECT;
         	    break;
 
-            case SM_DELIVER_OBJECT:
+            case SM_RIGHT_DELIVER_OBJECT:
 				std::cout << "State machine: SM_DELIVER_OBJECT" << std::endl;
 				
                 if(right_obj_index>=0){
                 	JustinaManip::raGoTo("take", 3000);
                 	JustinaTasks::dropObject("", false, 10000);
+                    right_obj_index=-1;
             	}
+                JustinaManip::hdGoTo(0.0, 0.0, 6000);
+                state=SM_FINAL_STATE;
+                break;
+            
+            case SM_LEFT_DELIVER_OBJECT:
+				std::cout << "State machine: SM_LEFT_DELIVER_OBJECT" << std::endl;
+				
             	if(left_obj_index>=0){
             		JustinaManip::laGoTo("take", 3000);
             		JustinaTasks::dropObject("", true, 10000);
+                    left_obj_index=-1;
             	}
      
                 JustinaManip::hdGoTo(0.0, 0.0, 6000);
-                //contdrink++;
-                state = SM_FINAL_STATE;
+                //if is same delivery place
+                    if(get_location(right_obj_type)==get_location(left_obj_type)){
+                        state = SM_RIGHT_DELIVER_OBJECT; 
+                    }else{
+                        state = SM_RIGHT_NAVIGATE_LOCATION; 
+                    }
 		        break;
                 
             case SM_FINAL_STATE:
@@ -199,7 +245,7 @@ int main(int argc, char** argv){
 			    std::cout << "I have finished test" << std::endl;	
         		success = true;
         		fail = true;
-                break;
+                return 0;
         }
         ros::spinOnce();
         loop.sleep();
